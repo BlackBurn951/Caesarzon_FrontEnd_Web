3f89d6b9e4629c5a56d0f62108d8011ed65cd6ed
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-04-09 18:36:37 +0200

    Modifica pop-up registrazione

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,30 @@
-import { Routes } from '@angular/router';
+import {RouterLink, RouterModule, RouterOutlet, Routes} from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {RegistrationComponent} from "./registration/registration.component";
+import {NgModule} from "@angular/core";
+import {MatDialogModule} from "@angular/material/dialog";
+
 
 export const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'registration', component: RegistrationComponent}
 ];
+
+@NgModule({
+  declarations: [RegistrationComponent, HomepageComponent],
+  imports: [RouterModule.forRoot(routes),
+    RouterOutlet,
+    RouterLink,
+    MatDialogModule
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+
+
+export class AppRoutes{
+
+
+
+}
