5e1a71b0d7e66770a539f3d21f676f425fcfad30
CesareGiroldini <cgiroldini@gmail.com>
2024-04-15 16:34:53 +0200

    Aggiunti header e footer predefiniti. Bozza della lista prodotti.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,14 @@ import {HomepageComponent} from "./homepage/homepage.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {NgModule} from "@angular/core";
 import {NgIf} from "@angular/common";
+import {LoginService} from "./loginService";
+import {ProductsListComponent} from "./products-list/products-list.component";
 
 
 export const routes: Routes = [
   {path: '', component: HomepageComponent},
-  {path: 'registration', component: RegistrationComponent}
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'products_list', component: ProductsListComponent}
 ];
 
 @NgModule({
