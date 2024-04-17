7dd963a86519c30cf3ff93ea49efd261c196c389
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-04-17 16:37:05 +0200

    Modifica aggiunta prodotto 2.1

--- src/app/product-management/product-management.component.ts
+++ src/app/product-management/product-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-product-management',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-management.component.css', '../../styles.css']
 })
 export class ProductManagementComponent {
+  prodottoForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.prodottoForm = this.fb.group({
+      nome: ['', Validators.required],
+      marca: ['', Validators.required],
+      descrizione: ['', Validators.required],
+      sconto: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
+      quantita: ['', [Validators.required, Validators.min(0)]],
+      prezzo: ['', [Validators.required, Validators.min(0)]],
+      coloreP: ['', Validators.required],
+      coloreS: ['', Validators.required],
+      taglia: ['none', Validators.required],
+      sport: ['', Validators.required],
+      categoria: ['', Validators.required]
+    });
+  }
+
+  aggiungiProdotto() {
+    // Implementazione per aggiungere il prodotto
+  }
+
+  get form() {
+    return this.prodottoForm.controls;
+  }
 
 }
